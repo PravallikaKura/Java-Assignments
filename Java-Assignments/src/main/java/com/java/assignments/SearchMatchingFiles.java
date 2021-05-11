950336acffaeb93b6651d692d8230ae03805a2ec
PravallikaKura <40121791+PravallikaKura@users.noreply.github.com>
2021-05-11 10:03:53 +0530

    Updated SearchMatchingFiles

--- Java-Assignments/src/main/java/com/java/assignments/SearchMatchingFiles.java
+++ Java-Assignments/src/main/java/com/java/assignments/SearchMatchingFiles.java
@@ -4,36 +4,54 @@ import java.io.File;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.SystemUtils;
 
+class FindFiles {
+	public void printFiles(String pattern) {
+		String homeDirectory = "/home/pravk";
+		File[] listOfFiles = new File(homeDirectory).listFiles();
+		printFilesByPath(listOfFiles,pattern);
+	}
+	
+	public void printFilesByPath(File[] listOfFiles, String pattern) {
+		if(listOfFiles == null)
+			return;
+		for(File file : listOfFiles) {
+			if(file.isDirectory()) {
+				File[] list = file.listFiles();
+				printFilesByPath(list, pattern);
+			} else {
+				Pattern patternTobeMatched = Pattern.compile(pattern);
+				Matcher matcher = patternTobeMatched.matcher(file.getName());
+				if(matcher.matches())
+					System.out.println("File Found " + file.getAbsolutePath());
+			}
+		}
+	}
+	
+	
+}
 
 public class SearchMatchingFiles {
-	
-	public ArrayList<String> isMatched(String path) {
-		
-		ArrayList<String> tokens = new ArrayList<String>();
-		String pattern = "/home*";
-		Pattern tokSplitter = Pattern.compile(pattern);
-		Matcher m = tokSplitter.matcher(pattern);
-		while (m.find()) {
-			tokens.add(m.group());
-		}
-		
-		return tokens;
-	}
 	public static void main(String args[]) throws Exception {
 
-			File userDir = SystemUtils.getUserDir();
-			System.out.println("User Directory: " + userDir.getCanonicalPath());
-
-			File userHome = SystemUtils.getUserHome();
-			System.out.println("User Home: " + userHome.getCanonicalPath());
-			
-			
-			
+		FindFiles findFilesMatched = new FindFiles();
+		Scanner sc = new Scanner(System.in);
+		String prompt;
+		String pattern = "^[a-zA-Z](.*).txt$";
+		
+		while(true) {
+			System.out.println("Enter pattern to seach home directory ");
+			prompt= sc.nextLine();
+			if(prompt.equalsIgnoreCase("Exit"))
+				break;
+			findFilesMatched.printFiles(pattern);
+		}
+		
 			
 			
 		}
