3bb8cf5a639e780f2c8a212afa451fbfa0605887
PravallikaKura <40121791+PravallikaKura@users.noreply.github.com>
2021-05-11 08:23:58 +0530

    Update CaseSensitivityCheck.java

--- Java-Assignments/src/main/java/com/java/assignments/CaseSensitivityCheck.java
+++ Java-Assignments/src/main/java/com/java/assignments/CaseSensitivityCheck.java
@@ -19,7 +19,8 @@ public class CaseSensitivityCheck {
 		int[] alphabets = new int[26];
 		int index,sum = 0;
 		
-		
+		//Time Complexity is O(n+26) ie n is the length of input string and as 26 is constant we can neglect that and in total O(n)
+		//Space Complexity is O(1) ie constant space is being used in whole program
 		
 		for(int i = 0;i < input.length();i++) {
 			index = ((int)(input.charAt(i)) - 97);
